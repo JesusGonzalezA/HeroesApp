586e5370b18d2a96d7cd26d0ebaaabc717da0390
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2021-01-05 16:23:42 +0100

    Test in login completed

---
+++ src/tests/components/login/LoginScreen.test.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { mount } from 'enzyme'
+
+import { LoginScreen } from "../../../components/login/LoginScreen"
+import { AuthContext } from '../../../auth/AuthContext';
+import { types } from '../../../types/types';
+
+describe('Testing Login component', () => {
+    
+    const history = {
+        replace: jest.fn()
+    }
+
+    const contextValues = {
+        dispatch: jest.fn(),
+        user: {
+            logged: false
+        }
+    }
+
+    const wrapper = mount(
+        <AuthContext.Provider value={contextValues}>
+            <LoginScreen history={ history }/>
+        </AuthContext.Provider>
+    )
+
+    test('should match the snapshot', () => {
+        expect( wrapper ).toMatchSnapshot();
+    })
+
+    test('should call dispatch and replace', () => {
+        wrapper.find('button').at(0).simulate('click');
+
+        expect( contextValues.dispatch ).toHaveBeenCalledWith({
+            type: types.login,
+            payload: {
+                name: 'Jesus',
+                email: 'jesus@mail.com'
+            }
+        });
+
+        expect( history.replace ).toHaveBeenCalled();
+    })
+    
+    
+})
