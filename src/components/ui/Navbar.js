35ae5b362bcd57f4cc27af35e87c77f48b8128bd
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2021-01-05 16:57:17 +0100

    Update title navbar

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -26,7 +26,7 @@ export const Navbar = () => {
                 className="navbar-brand" 
                 to="/"
             >
-                Asociaciones
+                HeroesApp
             </Link>
 
             <div className="navbar-collapse">
